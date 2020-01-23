7d99a44d5a827a2f1e2191c3dbc77bbfa0a478ce
tawhidabdullah <tawhidabdullah22@gmail.com>
2020-01-23 15:06:47 +0600

    pushing code with build

---
+++ build/precache-manifest.2d3621d59d6a6778405ef5385e7ed351.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c79946fa058dea21cc17931b50c25a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6122108297637e5f51b2",
+    "url": "/static/css/2.c5c53b0b.chunk.css"
+  },
+  {
+    "revision": "40b20853d0889dcd1617",
+    "url": "/static/css/main.42dc4eb4.chunk.css"
+  },
+  {
+    "revision": "6122108297637e5f51b2",
+    "url": "/static/js/2.6f546c05.chunk.js"
+  },
+  {
+    "revision": "ff8b52e736c7fa48a72effa31258a101",
+    "url": "/static/js/2.6f546c05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40b20853d0889dcd1617",
+    "url": "/static/js/main.121c762b.chunk.js"
+  },
+  {
+    "revision": "1cd4ae7df3e24fc38d10",
+    "url": "/static/js/runtime-main.2ad2c407.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "2e7f29752ee8ab8ae482146daa5c5323",
+    "url": "/static/media/spinner2.2e7f2975.gif"
+  }
+]);
